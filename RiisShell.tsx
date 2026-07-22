cbd439e16be4e74dc511b3e976f5c8511d46aa97
rajamuazzam <37911828+rajamuazzam@users.noreply.github.com>
2026-07-22 16:26:45 +0800

    Delete RiisShell.tsx

--- RiisShell.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = "force-static";
-
-export default function sitemap() {
-  return [
-    { url: "https://www.riis.engineering", lastModified: new Date("2026-07-17") },
-    { url: "https://www.riis.engineering/offshore/", lastModified: new Date("2026-07-17") },
-    { url: "https://www.riis.engineering/onshore/", lastModified: new Date("2026-07-17") },
-  ];
-}
