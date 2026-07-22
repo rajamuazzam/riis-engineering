3b92a4b12682dbe58544eb99c20206dd72bedbf6
rajamuazzam <37911828+rajamuazzam@users.noreply.github.com>
2026-07-22 10:44:44 +0800

    Delete app directory

--- app/robots.js
+++
@@ -1,5 +0,0 @@
-export const dynamic = "force-static";
-
-export default function robots() {
-  return { rules: { userAgent: "*", allow: "/" }, sitemap: "https://www.riis.engineering/sitemap.xml", host: "https://www.riis.engineering" };
-}
