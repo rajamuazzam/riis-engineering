989756210997dfea16b8d0a1415b777c69566d5f
rajamuazzam <37911828+rajamuazzam@users.noreply.github.com>
2026-07-22 10:45:09 +0800

    Delete public directory

--- public/offshore-native/script.js
+++
@@ -1,4 +0,0 @@
-const modal=document.querySelector(".modal");
-document.querySelectorAll(".js-contact").forEach(x=>x.addEventListener("click",()=>{modal.hidden=false;document.body.style.overflow="hidden"}));
-document.querySelectorAll(".js-close").forEach(x=>x.addEventListener("click",()=>{modal.hidden=true;document.body.style.overflow=""}));
-document.addEventListener("keydown",e=>{if(e.key==="Escape"&&!modal.hidden){modal.hidden=true;document.body.style.overflow=""}});
