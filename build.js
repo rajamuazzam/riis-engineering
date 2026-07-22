6798d3189c3c1f945a2ca40ce53ffbd0d03e9582
rajamuazzam <37911828+rajamuazzam@users.noreply.github.com>
2026-07-22 10:20:35 +0800

    Delete build.js

--- build.js
+++
@@ -1,8 +0,0 @@
-const fs=require("fs"),path=require("path");
-const out=path.join(__dirname,"out");
-fs.rmSync(out,{recursive:true,force:true});fs.mkdirSync(out,{recursive:true});
-for(const n of ["index.html","styles.css","script.js","_headers","robots.txt","sitemap.xml"]){
-  fs.copyFileSync(path.join(__dirname,n),path.join(out,n));
-}
-fs.cpSync(path.join(__dirname,"public"),path.join(out,"public"),{recursive:true});
-console.log("Native RIIS concept exported to out/");
